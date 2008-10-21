291bbc7999d3ff9e56f1877746b569a53e014238
Kunal Anand <kunal.r.anand@gmail.com>
2008-10-21 08:37:40 -0700

    Added a super minimal setup.sh and an icon

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 
 plist = dict(
     NSPrincipalClass='Trashie',
-    LSBackgroundOnly=True
+    LSBackgroundOnly=True,
+    CFBundleIconFile='trashie',
+    NSHumanReadableCopyright='Kunal Anand'
 )
 
 setup(
     app=["trashie.py"],
     options=dict(py2app=dict(
       plist=plist, 
-      resources='trash-empty.png, trash-full.png')),
+      resources='trash-empty.png, trash-full.png, trashie.icns')),
     setup_requires=["py2app"],
 )
